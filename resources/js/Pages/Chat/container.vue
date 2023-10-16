d7eea316fbc1ebb5c65f9cce2f784b8ce6a87f31
Bahadır Önal <bahadironal3@gmail.com>
2023-10-16 12:05:36 +0300

    finish commit

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -48,7 +48,29 @@ export default {
         }
     },
 
+    watch: {
+        currentRoom(val, oldVal) {
+            if (oldVal.id) {
+                this.disconnect(oldVal);
+            }
+            this.connect();
+        }
+    },
+
     methods: {
+        connect() {
+            if (this.currentRoom.id) {
+                let vm = this;
+                this.getMessages();
+                window.Echo.private("chat." + this.currentRoom.id)
+                .listen('.message.new', e => {
+                    vm.getMessages();
+                });
+            }
+        },
+        disconnect(room) {
+            window.Echo.leave("chat." + room.id);
+        },
         getRooms() {
             axios.get('/chat/rooms')
             .then( response => {
